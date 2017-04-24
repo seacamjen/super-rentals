2ef077db62008d78f05045e8889ba443f64aa5a4
Cameron Jensen and Nathaniel Meyer <nathaniel@>
2017-04-24 15:32:12 -0700

    add update functionality

--- app/routes/index.js
+++ app/routes/index.js
@@ -11,6 +11,16 @@ export default Ember.Route.extend({
       this.transitionTo('index');
     },
 
+    update(rental, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          rental.set(key.params[key]);
+        }
+      });
+      rental.save();
+      this.transitionTo('index');
+    },
+
     saveRental3(params) {
       var newRental = this.store.createRecord('rental', params);
       newRental.save();
