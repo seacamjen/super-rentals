aec293a865784bdd80b563b3a9f0250e091ce1c0
Cameron Jensen and Nathaniel Meyer <cameron@>
2017-04-24 16:03:56 -0700

    Add dynamic routing

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,23 +4,7 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('rental');
   },
-
   actions: {
-    destroyRental(rental) {
-      rental.destroyRecord();
-      this.transitionTo('index');
-    },
-
-    update(rental, params) {
-      Object.keys(params).forEach(function(key) {
-        if(params[key]!==undefined) {
-          rental.set(key.params[key]);
-        }
-      });
-      rental.save();
-      this.transitionTo('index');
-    },
-
     saveRental3(params) {
       var newRental = this.store.createRecord('rental', params);
       newRental.save();
